baf8c01e8331c9ca0d3712ccafcdf61d1d15e636
Pedro Lugon <pedromotta20@gmail.com>
2024-01-24 02:31:12 -0300

    feat(category): methods tests

--- src/domain/__tests__/category.entity.test.ts
+++ src/domain/__tests__/category.entity.test.ts
@@ -31,4 +31,62 @@ describe("Category Unit Tests", () => {
       expect(category.created_at).toBe(created_at);
     });
   });
+
+  describe("methods", () => {
+    test("should create a category", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBeTruthy();
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    test("should change category name", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+
+      category.changeName("TV Show");
+      expect(category.name).toBe("TV Show");
+    });
+
+    test("should change category description", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+
+      category.changeDescription("Movie description");
+      expect(category.description).toBe("Movie description");
+    });
+
+    test("should activate a category", () => {
+      const category = Category.create({
+        name: "Movie",
+        is_active: false,
+      });
+
+      expect(category.is_active).toBeFalsy();
+
+      category.activate();
+
+      expect(category.is_active).toBeTruthy();
+    });
+
+    test("should deactivate a category", () => {
+      const category = Category.create({
+        name: "Movie",
+        is_active: true,
+      });
+
+      expect(category.is_active).toBeTruthy();
+
+      category.deactivate();
+
+      expect(category.is_active).toBeFalsy();
+    });
+  });
 });
