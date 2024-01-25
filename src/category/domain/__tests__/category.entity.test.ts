542e8d247d57e97af23b54607f401ffa16b28800
Pedro Lugon <pedromotta20@gmail.com>
2024-01-24 22:47:22 -0300

    feat(category): uuid in category id

--- src/category/domain/__tests__/category.entity.test.ts
+++ src/category/domain/__tests__/category.entity.test.ts
@@ -1,3 +1,4 @@
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../category.entity";
 
 describe("Category Unit Tests", () => {
@@ -7,7 +8,7 @@ describe("Category Unit Tests", () => {
         name: "Movie",
       });
 
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBeNull();
       expect(category.is_active).toBeTruthy();
@@ -24,11 +25,31 @@ describe("Category Unit Tests", () => {
         created_at,
       });
 
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBe("Movie description");
       expect(category.is_active).toBeFalsy();
       expect(category.created_at).toBe(created_at);
+    });
+  });
+
+  describe("category_id field", () => {
+    const arrange = [
+      { category_id: null },
+      { category_id: undefined },
+      { category_id: new Uuid() },
+    ];
+    test.each(arrange)("id = %j", ({ category_id }) => {
+      const category = new Category({
+        name: "Movie",
+        category_id: category_id as any,
+      });
+
+      expect(category.category_id).toBeInstanceOf(Uuid);
+
+      if (category_id instanceof Uuid) {
+        expect(category.category_id).toBe(category_id);
+      }
     });
   });
 
@@ -38,7 +59,7 @@ describe("Category Unit Tests", () => {
         name: "Movie",
       });
 
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBeNull();
       expect(category.is_active).toBeTruthy();
